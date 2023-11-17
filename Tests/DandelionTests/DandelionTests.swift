317f95453aac0bd6c137ab04fef5e7a3e491edec
consuelita <adelita@operatorfoundation.org>
2023-11-17 13:55:24 -0600

    Update DandelionTests.swift

--- Tests/DandelionTests/DandelionTests.swift
+++ Tests/DandelionTests/DandelionTests.swift
@@ -267,6 +267,151 @@ final class DandelionTests: XCTestCase
             return
         }
     }
+    
+    func testConnectToDandelionServerFirst() async throws
+    {
+        let serverIP = "127.0.0.1"
+        let serverPort = 5771
+        let message1 = "Hello"
+        
+        // Get a shadow config
+        let testKeychainURL = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(".keychainTest")
+        
+        
+        // Use Shadow connection to make a Nametag connection
+        guard let keychain = Keychain(baseDirectory: testKeychainURL) else
+        {
+            XCTFail()
+            return
+        }
+        
+        guard let privateSigningKey = keychain.retrieveOrGeneratePrivateKey(label: "Nametag", type: KeyType.P256Signing) else
+        {
+            XCTFail()
+            return
+        }
+        
+        let publicKey = privateSigningKey.publicKey
+        
+        print("Initializing nametag. Public key is \(publicKey.data!.count) bytes.")
+        print("Nametag expected public key size is 65 bytes.") //Nametag.expectedPublicKeySize
+        
+        guard let _ = Nametag(keychain: keychain) else
+        {
+            XCTFail()
+            return
+        }
+        
+        print("• created a Nametag instance.")
+        
+        do
+        {
+            let testLog = Logger(subsystem: "TestLogger", category: "main")
+            
+            guard let connection = TCPConnection(host: serverIP, port: serverPort) else
+            {
+                XCTFail()
+                return
+            }
+            
+            let nametagConnection = try NametagClientConnection(connection, keychain, testLog)
+            
+            print("• Created a nametag connection.")
+            let wroteData = nametagConnection.network.write(string: message1)
+            print("• Wrote some data to the nametag/Dandelion connection.")
+            
+            guard wroteData else
+            {
+                XCTFail()
+                return
+            }
+            
+            connection.close()
+            try await Task.sleep(for: .seconds(2))
+        }
+        catch (let error)
+        {
+            print("• Failed to create a nametag connection: \(error)")
+            XCTFail()
+            return
+        }
+    }
+    
+    func testConnectToDandelionServerSecond() async throws
+    {
+        let serverIP = "127.0.0.1"
+        let serverPort = 5771
+        let message1 = "Hello"
+        let message2 = " Dandelion."
+        
+        // Get a shadow config
+        let testKeychainURL = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(".keychainTest")
+        
+        
+        // Use Shadow connection to make a Nametag connection
+        guard let keychain = Keychain(baseDirectory: testKeychainURL) else
+        {
+            XCTFail()
+            return
+        }
+        
+        guard let privateSigningKey = keychain.retrieveOrGeneratePrivateKey(label: "Nametag", type: KeyType.P256Signing) else
+        {
+            XCTFail()
+            return
+        }
+        
+        let publicKey = privateSigningKey.publicKey
+        
+        print("Initializing nametag. Public key is \(publicKey.data!.count) bytes.")
+        print("Nametag expected public key size is 65 bytes.") //Nametag.expectedPublicKeySize
+        
+        guard let _ = Nametag(keychain: keychain) else
+        {
+            XCTFail()
+            return
+        }
+        
+        print("• created a Nametag instance.")
+        
+        do
+        {
+            let testLog = Logger(subsystem: "TestLogger", category: "main")
+            
+            guard let connection = TCPConnection(host: serverIP, port: serverPort) else
+            {
+                XCTFail()
+                return
+            }
+            
+            let nametagConnection = try NametagClientConnection(connection, keychain, testLog)
+            
+            print("• Created a nametag connection.")
+            let wroteData = nametagConnection.network.write(string: message2)
+            print("• Wrote some data to the nametag/Dandelion connection.")
+            
+            guard wroteData else
+            {
+                XCTFail()
+                return
+            }
+                        
+            guard let readResult = nametagConnection.network.read(size: 16) else
+            {
+                XCTFail()
+                return
+            }
+            
+            print("Read from the nametag connection: \(readResult.string)")
+            XCTAssertEqual(message1 + message2, readResult.string)
+        }
+        catch (let error)
+        {
+            print("• Failed to create a nametag connection: \(error)")
+            XCTFail()
+            return
+        }
+    }
 
     func testConnectToDandelionServerTwiceAsync() async throws
     {
