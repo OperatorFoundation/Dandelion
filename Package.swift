004505c9e7e6b6acfbd3055b18b5d8a80006c154
bragelbytes <joebragel@gmail.com>
2023-12-10 17:27:44 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,12 +24,12 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
         .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
-        .package(url: "https://github.com/OperatorFoundation/Keychain", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/KeychainCli", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/Nametag", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/ShadowSwift", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/Straw", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionAsync", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Keychain", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/KeychainCli", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/Nametag", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/ShadowSwift", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/Straw", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionAsync", branch: "release"),
     ],
     
     targets: [
