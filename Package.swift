dd47f064a5201987cdfd886695252f03e276c0ec
bragelbytes <joebragel@gmail.com>
2024-03-22 16:17:35 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,14 +24,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
         .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
-        .package(url: "https://github.com/OperatorFoundation/Chord.git", from: "0.1.4"),
-        .package(url: "https://github.com/OperatorFoundation/Keychain", from: "1.0.2"),
-        .package(url: "https://github.com/OperatorFoundation/KeychainCli", from: "1.0.1"),
-        .package(url: "https://github.com/OperatorFoundation/Nametag", from: "0.1.2"),
-        .package(url: "https://github.com/OperatorFoundation/ShadowSwift", from: "5.0.2"),
-        .package(url: "https://github.com/OperatorFoundation/Straw", from: "1.0.1"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionAsync", from: "0.1.4"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionAsyncNametag", from: "1.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/Chord.git", from: "0.1.5"),
+        .package(url: "https://github.com/OperatorFoundation/Keychain", from: "1.0.3"),
+        .package(url: "https://github.com/OperatorFoundation/KeychainCli", from: "1.0.2"),
+        .package(url: "https://github.com/OperatorFoundation/Nametag", from: "0.1.3"),
+        .package(url: "https://github.com/OperatorFoundation/ShadowSwift", from: "5.0.3"),
+        .package(url: "https://github.com/OperatorFoundation/Straw", from: "1.0.4"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionAsync", from: "0.1.5"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionAsyncNametag", from: "1.0.1"),
     ],
     
     targets: [
